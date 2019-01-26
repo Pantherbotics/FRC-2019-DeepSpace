699080d874d380ad04b090f204fb37ced2e857cb
MatthewUnderscoreShen <MatthewShen02@Gmail.com>
2019-01-26 09:43:53 -0800

    Open loop is for rookies

--- src/main/java/frc/robot/commands/DriveClosedLoop.java
+++ src/main/java/frc/robot/commands/DriveClosedLoop.java
@@ -6,27 +6,32 @@ import frc.robot.util.Units;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class DriveClosedLoop extends Command {
+
   public DriveClosedLoop() {
     requires(Robot.kDrivetrain);
   }
 
+  @Override
   protected void initialize() {
   }
 
+  @Override
   protected void execute() { //generally choose this one
     double zoom = Robot.oi.getLeftYAxis(); //zoom = throttle
     double nyoom = Robot.oi.getRightXAxis(); //nyoom = steering
     Robot.kDrivetrain.setFPS(Units.FPSToTalonNative(zoom - nyoom), Units.FPSToTalonNative(zoom + nyoom));
-    
   }
 
+  @Override
   protected boolean isFinished() {
     return false;
   }
 
+  @Override
   protected void end() {
   }
-
+  
+  @Override
   protected void interrupted() {
   }
 }
