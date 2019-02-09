f3571b7fb6d7d1faaef49a0e27c83f7f23dd4297
MatthewUnderscoreShen <MatthewShen02@Gmail.com>
2019-02-08 17:59:21 -0800

    steve encoders busted

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -123,7 +123,7 @@ public class Drivetrain extends Subsystem {
     public void setFPS(double left, double right){
         mLeftA.set(ControlMode.Velocity, left);
         mRightA.set(ControlMode.Velocity, right);
-        //System.out.println("Left: " + getEncoderVelocity()[0] + "     Right: " + getEncoderVelocity()[1]);
+        System.out.println("Left: " + getEncoderVelocity()[0] + "     Right: " + getEncoderVelocity()[1]);
     }
 
     public double[] getEncoderVelocity(){ //0 for left, 1 for right
