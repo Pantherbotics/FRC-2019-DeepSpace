b07a3506cb4d1f22364c83943837273c2d2abdbd
MatthewUnderscoreShen <MatthewShen02@Gmail.com>
2019-02-11 23:31:45 -0800

    aaron die

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -10,8 +10,8 @@ import frc.robot.Constants;
 public class Arm extends Subsystem{
     double FF = 0;
     int timeout_ms = 0;
-    TalonSRX mTalonA = new TalonSRX(Constants.kArmA); //On carriage
-    TalonSRX mTalonB = new TalonSRX(Constants.kArmB); //On intake
+    TalonSRX mShoulder = new TalonSRX(Constants.kArmA); //On carriage
+    TalonSRX mWrist = new TalonSRX(Constants.kArmB); //On intake
 
     public Arm(){
         initPID();
@@ -19,19 +19,19 @@ public class Arm extends Subsystem{
 
     public void initPID(){
         //Near elevator joint
-        mTalonA.configSelectedFeedbackSensor(FeedbackDevice.Analog, Constants.armA_ID, timeout_ms);
-        mTalonA.configAllowableClosedloopError(Constants.armA_ID, 1, timeout_ms);
-        mTalonA.config_kP(Constants.armA_ID, Constants.armAKP, timeout_ms);
-        mTalonA.config_kI(Constants.armA_ID, Constants.armAKI, timeout_ms);
-        mTalonA.config_kD(Constants.armA_ID, Constants.armAKD, timeout_ms);
+        mShoulder.configSelectedFeedbackSensor(FeedbackDevice.Analog, Constants.armA_ID, timeout_ms);
+        mShoulder.configAllowableClosedloopError(Constants.armA_ID, 1, timeout_ms);
+        mShoulder.config_kP(Constants.armA_ID, Constants.armAKP, timeout_ms);
+        mShoulder.config_kI(Constants.armA_ID, Constants.armAKI, timeout_ms);
+        mShoulder.config_kD(Constants.armA_ID, Constants.armAKD, timeout_ms);
         //No kF allowed
         //Far elevator joint
-        mTalonB.configSelectedFeedbackSensor(FeedbackDevice.Analog, Constants.armB_ID, timeout_ms);
-        mTalonB.configAllowableClosedloopError(Constants.armB_ID, 1, timeout_ms);
-        mTalonB.config_kP(Constants.armB_ID, Constants.armBKP, timeout_ms);
-        mTalonB.config_kI(Constants.armB_ID, Constants.armBKI, timeout_ms);
-        mTalonB.config_kD(Constants.armB_ID, Constants.armBKD, timeout_ms);
-        mTalonB.config_kF(Constants.armB_ID, Constants.armBKF, timeout_ms);
+        mWrist.configSelectedFeedbackSensor(FeedbackDevice.Analog, Constants.armB_ID, timeout_ms);
+        mWrist.configAllowableClosedloopError(Constants.armB_ID, 1, timeout_ms);
+        mWrist.config_kP(Constants.armB_ID, Constants.armBKP, timeout_ms);
+        mWrist.config_kI(Constants.armB_ID, Constants.armBKI, timeout_ms);
+        mWrist.config_kD(Constants.armB_ID, Constants.armBKD, timeout_ms);
+        mWrist.config_kF(Constants.armB_ID, Constants.armBKF, timeout_ms);
     }
     
     public void powerArm(double input){
@@ -39,20 +39,20 @@ public class Arm extends Subsystem{
     }
 
     public int getPosA(){
-        return (mTalonA.getSelectedSensorPosition(0) + Constants.offsetA); //Flat should be 0
+        return (mShoulder.getSelectedSensorPosition(0) + Constants.offsetA); //Flat should be 0
     }
 
     public int getPosB(){
-        return (mTalonB.getSelectedSensorPosition(0) + Constants.offsetB);
+        return (mWrist.getSelectedSensorPosition(0) + Constants.offsetB);
     }
 
     public void setPosA(int position){
-        FF = Math.cos(Constants.armAKF);
-        mTalonA.set(ControlMode.MotionMagic, position + Constants.offsetA, DemandType.ArbitraryFeedForward, FF);
+        FF = Constants.armAKF * Math.cos(Constants.encoder2Rad * (position + Constants.offsetA));
+        mShoulder.set(ControlMode.MotionMagic, position + Constants.offsetA, DemandType.ArbitraryFeedForward, FF);
     }
 
     public void setPosB(int position){
-        mTalonB.set(ControlMode.MotionMagic, position + Constants.offsetB);
+        mWrist.set(ControlMode.MotionMagic, position + Constants.offsetB);
     }
 
     public void initDefaultCommand(){
