5eb4424fccb53ab4a78a666863bf36388058560d
MatthewUnderscoreShen <MatthewShen02@Gmail.com>
2019-04-05 14:02:09 -0700

    surprisingly difficult, yet surprisingly easy

--- src/main/java/frc/robot/util/tdd/TestElevator.java
+++ src/main/java/frc/robot/util/tdd/TestElevator.java
@@ -1,36 +1,44 @@
+//3863 Pantherbotics's attempt at replicating Test Driven Development
+
 package frc.robot.util.tdd;
 
 public class TestElevator {
     //775 Info
     //Stall Torque (Nm) = Amount of force needed to stall the motor
-    static double kStallTorque = 0.71;
+    static final double kStallTorque = 0.71;
     //Stall Current (A) = Current drawn when motor is stalled
-    static double kStallCurrent = 134;
+    static final double kStallCurrent = 134;
     //Free Speed (RPM) = Maximum speed under no load
-    static double kFreeSpeed = 18730;
+    static final double kFreeSpeed = 18730;
     //Free Current (A) = Maximum current under no load
-    static double kFreeCurrent = 0.7;
+    static final double kFreeCurrent = 0.7;
 
     //Elevator Info
     //Mirage has 2 775s on the elevator gearbox
-    static double kMotors = 2.0;
+    static final double kMotors = 2.0;
     //Motor Resistance
-    static double kResistance = 12.0 / kStallCurrent;
+    static final double kResistance = 12.0 / kStallCurrent;
     //Mass (8+5)lbs = 5.8967kg
-    static double kMass = 5.8967;
+    static final double kMass = 5.8967;
     //Gear Ratio (775) 13.33:1 (Spool)
-    static double kGR = 1/13.33;
-    //Spool Diameter = 1.25in
-    static double kR = 1.25 / 2;
+    static final double kGR = 1/13.33;
+    //Spool Diameter = 1.25in ==> 0.03175m
+    static final double kR = 0.03175 / 2; //(m)
     //Torque Constant
-    static double kT = kMotors * kStallTorque / kStallCurrent;
+    static final double kT = kMotors * kStallTorque / kStallCurrent;
     //Motor Velocity Constant (W No Load / V Peak)
-    static double kV = kFreeSpeed / (kFreeCurrent * kResistance);
+    static final double kV = kFreeSpeed / (kFreeCurrent * kResistance);
     //Friction = A constant that is subtracted from velocity
-    static double kFriction = 0.0;
+    static final double kFriction = 0.0;
 
-    private static double getAccel(double voltage){ //V = IR + W/kV, Torque = kT * I;
-        return 0;
+    //Velocity (m/s)
+    public static double velocity = 0;
+    //Position (m)
+    public static double position = 0;
+
+    //acceleration of the elevator
+    private static double getAccel(double voltage){ //
+        return kGR * kT * (voltage - kGR * velocity /(kR * kV)) / (kResistance * kMass * kR);
     }
 
     public void runSim(double voltage, double time){
@@ -39,6 +47,10 @@ public class TestElevator {
 
         while(currentTime < time){
             currentTime += simTime;
+
+            double acceleration = getAccel(voltage);
+            position += simTime * velocity; //Austin Schuh says this order big good
+            velocity += simTime * acceleration - kFriction;
         }
     }
 }
